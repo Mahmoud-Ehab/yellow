14c305f386ba2f32718bf7476579d1b30d527c30
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-07-20 08:19:38 +0300

    electron-is-dev installed and used in loadURL func

--- electron.mjs
+++ electron.mjs
@@ -1,19 +1,21 @@
 // electron.js
 import { app, BrowserWindow } from 'electron'
 import { startServer, closeServer } from './js/server.mjs'
+import isDiv from 'electron-is-dev'
 
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       nodeIntegration: true,
     },
   });
-
-  const startURL = 'http://localhost:8081'
+  
+  // TODO: add the built html file location
+  const startURL = isDiv ? 'http://localhost:8081' : "file://" 
   mainWindow.loadURL(startURL);
   mainWindow.on('closed', () => (mainWindow = null));
 }
@@ -23,8 +25,8 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-    closeServer();
   }
+  closeServer();
 });
 
 app.on('activate', () => {
