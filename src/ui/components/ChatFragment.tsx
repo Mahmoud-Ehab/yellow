08320465db12a8e132965652a650d31bbbd80373
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-07-23 08:49:49 +0300

    reconnect button has been implemented.

--- src/ui/components/ChatFragment.tsx
+++ src/ui/components/ChatFragment.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { View, Text, ScrollView } from "react-native";
 import { Image } from "expo-image";
 import { List } from "react-native-paper";
@@ -15,6 +15,28 @@ import { getGlobal } from "../../inits/globals.init"
 export function ChatFragment({ username, ipaddr }) {
     const style = getChatFragmentStyle();
     const [pulv, setPulv] = useState(false); // pulv: PopUp List Visibility
+    const [connected, setConnected] = useState(false);
+
+    useEffect(() => {
+      reconnect()
+    }, [])
+
+    const reconnect = async () => {
+      notifier.notify({text: `Trying to reach ${ipaddr} in 10 seconds...`, type: "warning"});
+      fetch(`http://${ipaddr}:5000`, { signal: AbortSignal.timeout(10000) })
+      .then(res => res.json())
+      .then(payload => {
+        if (!payload.response.ipaddr) {
+          throw Error("not a Yellow server!");
+        }
+        setConnected(true);
+        notifier.notify({text: `${ipaddr} is listening.`, type: "success"});
+      })
+      .catch(err => {
+        setConnected(false)
+        notifier.notify({text: `couldn't reach ${ipaddr}`, type: "error"});
+      })
+    }
 
     const closeChatHandler = () => {
       const closeChat = getGlobal("closeChatFunc");
@@ -54,8 +76,9 @@ export function ChatFragment({ username, ipaddr }) {
             <View style={style.topBar}>
                 <Image 
                     style={style.conStatusBtn} 
-                    source={"../../../assets/disconnected.png"} 
+                    source={`../../../assets/${connected ? 'connected' : 'disconnected'}.png`} 
                     contentFit="contain"
+                    onPointerDown={reconnect}
                 />
                 <Text style={style.topBarText}>{ username }</Text>
                 <Image 
@@ -89,9 +112,7 @@ export function ChatFragment({ username, ipaddr }) {
                     style={style.sendBtn} 
                     source={"../../../assets/send.png"} 
                     contentFit="contain"
-                />
-            </View>
-
+                /> </View>
             { pulv ?
             <List.Section style={style.popUpView}>
                 <List.Item 
